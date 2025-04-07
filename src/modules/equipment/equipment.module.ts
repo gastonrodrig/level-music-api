e26893c8e6d745dde92459f5ad16b4ecbc7aab58
Gaston <gaston.rodriguez@icloud.com>
2025-04-06 23:42:06 -0500

    equipment module changed

--- src/modules/equipment/equipment.module.ts
+++ src/modules/equipment/equipment.module.ts
@@ -1,23 +1,21 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Equipment, EquipmentSchema } from './schema/equipment.schema';
-import { EquipmentService } from './services/equipment.service';
-import { EquipmentController } from './controllers/equipment.controller';
-import { EquipmentMaintenanceService } from './services/equipment_maintenance.service';
-import { EquipmentMaintenanceController } from './controllers/equipment_maintenance.controller';
-import {
-  EquipmentMaintenance,
-  EquipmentMaintenanceSchema,
-} from './schema/equipment_maintenance.schema';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Equipment, EquipmentSchema } from "./schema/equipment.schema";
+import { EquipmentService } from "./services/equipment.service";
+import { EquipmentController } from "./controllers/equipment.controller";
+import { EquipmentMaintenance, EquipmentMaintenanceSchema } from "./schema/equipment_maintenance.schema";
+import { EquipmentMaintenanceService } from "./services/equipment_maintenance.service";
+import { EquipmentMaintenanceController } from "./controllers/equipment_maintenance.controller";
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: Equipment.name, schema: EquipmentSchema },
-      { name: EquipmentMaintenance.name, schema: EquipmentMaintenanceSchema },
-    ]),
-  ],
-  providers: [EquipmentService, EquipmentMaintenanceService],
-  controllers: [EquipmentController, EquipmentMaintenanceController],
+    imports: [
+        MongooseModule.forFeature([
+          { name: Equipment.name, schema: EquipmentSchema },
+          { name: EquipmentMaintenance.name, schema: EquipmentMaintenanceSchema },
+        ])
+      ],
+      providers : [EquipmentService, EquipmentMaintenanceService],
+      controllers: [EquipmentController, EquipmentMaintenanceController]
+
 })
 export class EquipmentModule {}
