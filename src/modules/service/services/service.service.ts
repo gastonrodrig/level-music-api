b9ccfc24e3a07dd3926bd7675b98bfa78020ac37
Huayta <jdavidhuay@gmail.com>
2025-09-08 11:12:33 -0500

    services

--- src/modules/service/services/service.service.ts
+++ src/modules/service/services/service.service.ts
@@ -5,12 +5,18 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Service } from '../schema';
+import { Service, ServiceDetail } from '../schema';
 import { Model } from 'mongoose';
 import { Provider } from '../../provider/schema';
 import { ServiceType } from '../schema';
-import { CreateServiceDto, UpdateServiceDto } from '../dto';
+import { CreateServiceDto, UpdateServiceDto, CreateServiceDetailDto } from '../dto';
 import { SF_SERVICE } from 'src/core/utils';
+import { ServiceDetailMedia } from '../../uploads/schema/collection';
+import { stat } from 'fs';
+import { Estado } from 'src/core/constants/app.constants';
+import { parseServices, SF_FEATURED_EVENT, toObjectId } from 'src/core/utils';
+import { StorageService } from '../../firebase/services';
+import { UploadResult } from 'src/core/interfaces';
 
 @Injectable()
 export class ServiceService {
@@ -21,10 +27,20 @@ export class ServiceService {
     private providerModel: Model<Provider>,
     @InjectModel(ServiceType.name)
     private serviceTypeModel: Model<ServiceType>,
+    @InjectModel(ServiceDetailMedia.name)
+    private serviceMediaModel: Model<ServiceDetailMedia>,
+    @InjectModel(ServiceDetail.name)
+    private serviceDetailModel: Model<ServiceDetail>,
+    private readonly storageService: StorageService,
+    
   ) {}
 
   // Guiarse de aqui, solo cuando la tabla tiene ref's
-  async create(createServiceDto: CreateServiceDto): Promise<Service> {
+  async create(
+    createServiceDto: CreateServiceDto,
+    createServiceDetailDto: CreateServiceDetailDto,
+    media: Express.Multer.File[] = [],
+  ): Promise<{ service: Service; detail: ServiceDetail; multimedia: ServiceDetailMedia[] }> {
     try {
       const provider = await this.providerModel.findById(createServiceDto.provider_id);
       if (!provider) throw new BadRequestException('Provider not found');
@@ -32,15 +48,45 @@ export class ServiceService {
       const serviceType = await this.serviceTypeModel.findById(createServiceDto.service_type_id);
       if (!serviceType) throw new BadRequestException('Service type not found');
       
-      const newService = new this.serviceModel({
+      const newService = await this.serviceModel.create({
         ...createServiceDto,
+        status: Estado.ACTIVO,
         provider: provider._id,
         service_type: serviceType._id,
+        created_at: new Date(),
+        updated_at: new Date(),
         provider_name: provider.name,
         service_type_name: serviceType.name,
       });
+      
+      const upload = await this.storageService.uploadMultipleFiles(
+       'service-detail',
+        media,
+        'multimedia',
+      )as UploadResult[];
+      const detail = await this.serviceDetailModel.create({
+        ...createServiceDetailDto,
+        service_id: newService._id,
+        multimedia: [],
+      });
+      const multimedia = upload.map((file) => ({
+        url: file.url,
+        name: file.name,
+        size: file.size,
+        storagePath: file.storagePath,
+        detail_id: detail._id,
+        created_at: new Date(),
+      }));
+       await this.serviceMediaModel.insertMany(multimedia);
 
-      return await newService.save();
+      detail.multimedia = multimedia;
+      await detail.save();
+
+      return{
+        service: await newService.save(),
+        detail,
+        multimedia,
+      };
     } catch (error) {
       throw new InternalServerErrorException(
         `Error creating service: ${error.message}`,
