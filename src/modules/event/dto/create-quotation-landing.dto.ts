4d4138103b5406b946350ca8f3963de169220325
Huayta <jdavidhuay@gmail.com>
2025-10-02 21:37:39 -0500

    cambios

--- src/modules/event/dto/create-quotation-landing.dto.ts
+++ src/modules/event/dto/create-quotation-landing.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IntersectionType } from '@nestjs/swagger';
-import { ValidateNested } from 'class-validator';
+import { ValidateNested, IsBoolean, IsOptional  } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CreateEventDto, ClientInfoDto } from '.';
+
 
 export class CreateQuotationLandingDto extends IntersectionType(CreateEventDto) {
   @ApiProperty({ type: ClientInfoDto })
   @ValidateNested()
   @Type(() => ClientInfoDto)
   client_info: ClientInfoDto;
+  @IsOptional()
+  @IsBoolean()
+  is_quotation?: boolean;
 }
